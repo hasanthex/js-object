49fb755cdffd9e657d63e03dc4ce86f76adda2b3
Muhammad Hasan <hasan18353@gmail.com>
2018-10-21 17:36:01 +0600

    Create create.js

---
+++ create.js
@@ -0,0 +1,21 @@
+// Create Object, Using the Object() constructor:
+var obj_1 = new Object();
+
+// Create Object, Using Object.create() method:
+var obj_2 = Object.create(null);
+
+// Create Object, Using the bracket's syntax
+var obj_3 = {};
+
+// Create Object, Using a function constructor
+var obj_4 = function(){}
+
+
+// Create Object, Using the function constructor + prototype: 
+var obj_5 = function(){}
+obj_5.prototype.name  = 'Muhammad hasan';
+obj_5.prototype.email = 'hasan@email.oom';
+obj_5.prototype.show  = function(){console.log(this.name+' - '+this.email);}
+
+var result = new obj_5();
+result.show();
