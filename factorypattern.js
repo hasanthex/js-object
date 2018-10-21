946004aa5b9b11488b76997bc9c1b399917d5d95
Muhammad Hasan <hasan18353@gmail.com>
2018-10-21 18:22:08 +0600

    Create factorypattern.js

---
+++ factorypattern.js
@@ -0,0 +1,21 @@
+var factoryPattern = function(name, email, contact){
+  
+  // empty object
+  var obj = {};
+  
+  obj.name = name;
+  obj.email = email;
+  obj.contact = contact;
+  
+  obj.show = function(){
+    console.log(obj.name+' - '+obj.email+' - '+obj.contact);
+  }
+  
+  return obj;
+}
+
+
+var fact1 = new factoryPattern('muhammad', 'email@to.com', '123456');
+var fact2 = new factoryPattern('hasan', 'hasan@email.com', '987654');
+fact1.show();
+fact2.show();
